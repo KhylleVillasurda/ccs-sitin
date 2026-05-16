466c7ec4aa019d47922c011815c36a7ae6cf0ab8
KhylleVillasurda <freakyvader@proton.me>
2026-05-16 09:26:25 +0800

    feat(seeders): updated the seeders to follow the ID format

--- backend/src/seeders.rs
+++ backend/src/seeders.rs
@@ -23,26 +23,27 @@ impl Fairing for SeederFairing {
         };
 
         let seed_data = vec![
-            ("1001", "Emilia", "Phantasy Star", "https://i.imgur.com/example1.jpg"),
-            ("1002", "Patton", "WW2 General", "https://i.imgur.com/example2.jpg"),
-            ("1003", "Son Goku", "Dragon Ball", "https://i.imgur.com/example3.jpg"),
-            ("1004", "Laios", "Dungeon Meshi", "https://i.imgur.com/example4.jpg"),
-            ("1005", "Clare", "Claymore", "https://i.imgur.com/example5.jpg"),
+            ("23760451", "Emilia", "BSIT", "https://i.imgur.com/example1.jpg", 25),
+            ("23760452", "Patton", "BSCS", "https://i.imgur.com/example2.jpg", 15),
+            ("23760453", "Goku", "BSIT", "https://i.imgur.com/example3.jpg", 30),
+            ("23760454", "Laios", "BSCpE", "https://i.imgur.com/example4.jpg", 10),
+            ("23760455", "Clare", "BSCS", "https://i.imgur.com/example5.jpg", 20),
         ];
 
         let hashed = bcrypt::hash("password123", bcrypt::DEFAULT_COST).unwrap();
 
-        for (id, name, course, pfp) in seed_data {
+        for (id, name, course, pfp, sessions) in seed_data {
             sqlx::query(
                 "INSERT IGNORE INTO users
-                    (id_number, last_name, first_name, password, course, profile_picture, role)
-                 VALUES (?, ?, 'User', ?, ?, ?, 'student')"
+                    (id_number, last_name, first_name, password, course, profile_picture, role, remaining_sessions)
+                 VALUES (?, ?, 'Test', ?, ?, ?, 'student', ?)"
             )
             .bind(id)
             .bind(name)
             .bind(&hashed)
             .bind(course)
             .bind(pfp)
+            .bind(sessions)
             .execute(pool).await.ok();
         }
 
